af34f675b8539ec0f96c1bcd52f2511bf0bb5fb6
ajignacio <alvinjohn.ignacio@yahoo.com>
2017-04-05 17:35:29 +0800

    add cuisine data for rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+cuisine_data = ["Filipino","Chinese","American","Korean","Fastfood"]
+cuisine_data.each do |c|
+  Cuisine.create(name: c)
+end
